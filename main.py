1b0e11caa443f02fde06b511c886de4df6f1e661
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2022-07-14 20:04:01 +0200

    Calculate the average temperature with sum() and len()

--- main.py
+++ main.py
@@ -10,3 +10,6 @@ print(data_dict)
 # Cherry-Picking: extract a list for a specific element
 temp_list = data["temp"].to_list()
 print(temp_list)
+
+average_temp = sum(temp_list)/len(temp_list)
+print(f"The average temperature is {round(average_temp,2)}°C")
